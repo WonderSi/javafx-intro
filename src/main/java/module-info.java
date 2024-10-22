8e4b5362c590d75100435ab27ab9a32198ef3319
WonderSi <kirillkozlov00@gmail.com>
2024-10-23 00:29:39 +0700

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.javafxintro {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javafx.graphics;
+
+    opens org.example.javafxintro to javafx.fxml;
+    exports org.example.javafxintro;
+}
